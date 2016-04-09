a422ace18216a9be0777b701bc1568e62e670a44
Andres Mena <andres.mena.arias@intel.com>
2016-04-09 10:58:02 -0600

    initial commit

---
+++ TextMachineLearning/TextMachineLearning/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextMachineLearning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbd = new OpenFileDialog();
+            DialogResult result = fbd.ShowDialog();
+            textBox1.Text = fbd.FileName;
+        }
+    }
+}
